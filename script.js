6dfcfbb5437b8a1b2bcaf41e4011c214cc5c616d
Jeff Wong <jeffmjwong@gmail.com>
2018-05-17 13:58:32 +1000

    Solve AJAX challenge

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
 function addOne(number, callback) {
-  console.log('Started addOne...');
   result = number + 1;
   setTimeout(function() {
-    console.log('Invoking callback...')
     callback(result);
   }, 2000);
-  console.log('Finished addOne...');
 }
 
-function showResult(value) {
-  console.log(value);
-}
-
-console.log('Started main...');
-addOne(5, showResult);
-console.log('Finished main...');
+addOne(5, value => console.log(value));
